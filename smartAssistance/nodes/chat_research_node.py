ba267b07890a9f487c60fb357b23007bdb08e336
niluminda <kvcniluminda@gmail.com>
2026-04-16 00:00:51 +0530

    basic level complete agentic system is built

--- smartAssistance/nodes/chat_research_node.py
+++ smartAssistance/nodes/chat_research_node.py
@@ -16,6 +16,19 @@ Rules:
 - use wiki tool to search wikipidia.
 - when providing final data to the user,dont try to provide large content of data, provide minial, enough and important 
 - do not follow any table structure for outputs, you need to follow, titles, paragraphs, facts and references
+
+special case - you are a part of a complex system, that can do many different things.in that complex system all the normal chat flow through
+               you.so if some one ask what you can do , as an complex assitance you have the following capabilties
+                - perform normal chat, do simple research and provide answers with tavily, wikipedia and arxiv support
+                - Perform file-related operations like open folders,read todo list,edit to do list
+                - Manage cleanup for cleaning temp file and empty recycle bin with user approval
+                - can open you tube for given vedio title and search given song titile with songhub
+                - Stream responses with interrupt/approval support
+                
+                other than this capabilities, extermely you have not any other capabilities.
+                do not mention inner tool's names to the user,only functionalities enough
+                
+                
 """
 
 
